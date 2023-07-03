cdb0f08ac1c1afe4c978a00877cf4f67961896d7
wangjinjin <wangjinjin@didiglobal.com>
2023-07-03 19:29:20 +0800

    init

---
+++ src/main/java/leetcode/array/No1991.java
@@ -0,0 +1,12 @@
+package leetcode.array;
+
+/**
+ * @author wangjinjin@didiglobal.com
+ * @create 2023-07-03 19:28
+ */
+public class No1991 {
+    public static void main(String[] args) {
+
+    }
+
+}
