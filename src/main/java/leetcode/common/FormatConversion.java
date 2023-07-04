d6aa054b4aba9a6433e615be4c36e7a3d0fd70f6
wangjinjin <wangjinjin@didiglobal.com>
2023-07-04 16:07:09 +0800

    1991

--- src/main/java/leetcode/common/FormatConversion.java
+++ src/main/java/leetcode/common/FormatConversion.java
@@ -1,7 +1,26 @@
-package leetcode.common;/**
- * 
- * 
- * @author wangjinjin@didiglobal.com
+package leetcode.common;
+
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 测试数据格式转换
+ *
  * @create 2023-07-04 15:43
- */public class FormatConversion {
+ */
+public class FormatConversion {
+
+    /**
+     * @param str
+     * @return
+     */
+    public static int[] strToIntArray(String str) {
+        str = StringUtils.removeStart(str, "[");
+        str = StringUtils.removeEnd(str, "]");
+        str = StringUtils.remove(str, " ");
+        String[] split = str.split(",");
+        return Arrays.stream(split).map(Integer::parseInt).mapToInt(Integer::intValue).toArray();
+    }
+
 }
--- src/main/java/leetcode/type01_array/No1991.java
+++ src/main/java/leetcode/type01_array/No1991.java
@@ -1,12 +1,47 @@
-package leetcode.n1_array;
+package leetcode.type01_array;
+
+import static leetcode.common.FormatConversion.strToIntArray;
 
 /**
- * @author wangjinjin@didiglobal.com
+ * 1991. Find the Middle Index in Array
+ *
  * @create 2023-07-03 19:28
  */
 public class No1991 {
     public static void main(String[] args) {
+        // 3
+        System.out.println(findMiddleIndex(strToIntArray("[2,3,-1,8,4]")));
+        // 2
+        System.out.println(findMiddleIndex(strToIntArray("[1,-1,4]")));
+        // -1
+        System.out.println(findMiddleIndex(strToIntArray("[2,5]")));
+    }
 
+    /**
+     * 思路：先求和，然后遍历数组，同时记录左右数组和
+     * 复杂度：2n
+     *
+     * @param nums
+     * @return
+     */
+    public static int findMiddleIndex(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        int left = 0;
+        int right = sum - nums[0];
+        for (int j = 0; j < nums.length; j++) {
+            if (left == right) {
+                return j;
+            }
+            if (j >= nums.length - 1) {
+                break;
+            }
+            left += nums[j];
+            right -= nums[j + 1];
+        }
+        return -1;
     }
 
 }
