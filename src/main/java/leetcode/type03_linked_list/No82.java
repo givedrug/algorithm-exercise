cbeca099049efcd1b21608459320881592ab9b9b
givedrug <givedrug@outlook.com>
2023-07-14 23:35:24 +0800

    82

---
+++ src/main/java/leetcode/type03_linked_list/No82.java
@@ -0,0 +1,53 @@
+package leetcode.type03_linked_list;
+
+import leetcode.common.DataStructureDefinition.ListNode;
+
+import static leetcode.common.FormatConversion.strToListNode;
+import static leetcode.common.FormatOutput.printListNode;
+
+/**
+ * 82. Remove Duplicates from Sorted List II
+ *
+ * @author <a href="https://github.com/givedrug">givedrug</a>
+ * @create 2023-07-14 23:08
+ */
+public class No82 {
+
+    public static void main(String[] args) {
+        // [1,2,5]
+        printListNode(deleteDuplicates(strToListNode("[1,2,3,3,4,4,5]")));
+        // [2,3]
+        printListNode(deleteDuplicates(strToListNode("[1,1,1,2,3]")));
+        // [1,2,3]
+        printListNode(deleteDuplicates(strToListNode("[1,2,3,4,4,4]")));
+        // []
+        printListNode(deleteDuplicates(strToListNode("[1,1]")));
+        // [1]
+        printListNode(deleteDuplicates(strToListNode("[1]")));
+        // []
+        printListNode(deleteDuplicates(strToListNode("[]")));
+    }
+
+    /**
+     * 思路：快慢指针+虚拟头结点
+     * 复杂度：n
+     */
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode left = dummyHead, right = head;
+        while (right != null && right.next != null) {
+            if (right.next.val == left.next.val) {
+                while (right.next != null && right.next.val == left.next.val) {
+                    right = right.next;
+                }
+                left.next = right.next;
+            } else {
+                left = left.next;
+            }
+            right = right.next;
+        }
+        return dummyHead.next;
+    }
+
+}
