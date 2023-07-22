d07646d188a4f93468d60827aab24d5e1773fbec
givedrug <givedrug@outlook.com>
2023-07-22 13:15:16 +0800

    876

---
+++ src/main/java/leetcode/type03_linked_list/No876.java
@@ -0,0 +1,46 @@
+package leetcode.type03_linked_list;
+
+import leetcode.common.DataStructureDefinition.ListNode;
+
+import static leetcode.common.FormatConversion.strToListNode;
+import static leetcode.common.FormatOutput.printListNode;
+
+/**
+ * 876. Middle of the Linked List
+ *
+ * @author <a href="https://github.com/givedrug">givedrug</a>
+ * @create 2023-07-22 13:05
+ */
+public class No876 {
+
+    public static void main(String[] args) {
+        // [3,4,5]
+        printListNode((middleNode(strToListNode("[1,2,3,4,5]"))));
+        // [4,5,6]
+        printListNode((middleNode(strToListNode("[1,2,3,4,5,6]"))));
+        // [2]
+        printListNode((middleNode(strToListNode("[1,2]"))));
+        // [1]
+        printListNode((middleNode(strToListNode("[1]"))));
+        // []
+        printListNode((middleNode(strToListNode("[]"))));
+    }
+
+    /**
+     * 思路：快慢指针
+     * 复杂度：n
+     */
+    public static ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+}
