c33d235abc47c30ee6cea2ab5fa30d1986ffca7e
givedrug <givedrug@outlook.com>
2023-07-23 20:10:00 +0800

    25

---
+++ src/main/java/leetcode/type09_sliding_window/No25.java
@@ -0,0 +1,25 @@
+package leetcode.type09_sliding_window;
+
+import leetcode.common.DataStructureDefinition.ListNode;
+
+/**
+ * 25. Reverse Nodes in k-Group
+ *
+ * @author <a href="https://github.com/givedrug">givedrug</a>
+ * @create 2023-07-23 12:27
+ */
+public class No25 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 思路：
+     * 复杂度：
+     */
+    public static ListNode reverseKGroup(ListNode head, int k) {
+
+    }
+
+}
