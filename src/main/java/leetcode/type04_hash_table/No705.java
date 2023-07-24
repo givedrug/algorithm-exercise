c6ef6ba8f3b58ca16b4e03dcb3d21d9cec35194b
givedrug <givedrug@outlook.com>
2023-07-24 15:43:39 +0800

    705

---
+++ src/main/java/leetcode/type04_hash_table/No705.java
@@ -0,0 +1,39 @@
+package leetcode.type04_hash_table;
+
+/**
+ * 705. Design HashSet
+ *
+ * @author <a href="https://github.com/givedrug">givedrug</a>
+ * @create 2023-07-24 15:42
+ */
+public class No705 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 思路：
+     * 复杂度：
+     */
+    class MyHashSet {
+
+        public MyHashSet() {
+
+        }
+
+        public void add(int key) {
+
+        }
+
+        public void remove(int key) {
+
+        }
+
+        public boolean contains(int key) {
+
+        }
+
+    }
+
+}
