fdc311dbb4b3253cb51a9d371e81d54bb3f6be93
givedrug <givedrug@outlook.com>
2023-07-24 14:38:40 +0800

    25

--- src/main/java/leetcode/type09_sliding_window/No25.java
+++ src/main/java/leetcode/type09_sliding_window/No25.java
@@ -2,6 +2,9 @@ package leetcode.type09_sliding_window;
 
 import leetcode.common.DataStructureDefinition.ListNode;
 
+import static leetcode.common.FormatConversion.strToListNode;
+import static leetcode.common.FormatOutput.printListNode;
+
 /**
  * 25. Reverse Nodes in k-Group
  *
@@ -11,15 +14,74 @@ import leetcode.common.DataStructureDefinition.ListNode;
 public class No25 {
 
     public static void main(String[] args) {
-
+        // [2,1,4,3,5]
+        printListNode(reverseKGroup(strToListNode("[1,2,3,4,5]"), 2));
+        // [3,2,1,4,5]
+        printListNode(reverseKGroup(strToListNode("[1,2,3,4,5]"), 3));
     }
 
     /**
-     * 思路：
-     * 复杂度：
+     * 思路：k个一组，翻转
+     * 复杂度：n
      */
     public static ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // 虚拟头
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode pos = dummyHead;
+        while (pos != null) {
+            ListNode start = pos;
+            int count = 0;
+            while (count < k && pos != null) {
+                pos = pos.next;
+                count++;
+            }
+            if (pos == null) {
+                break;
+            }
+            if (count == k) {
+                // 剩余长度足够k时
+                ListNode next = pos.next;
+                ListNode oldHead = start.next;
+                pos.next = null;
+                ListNode newHead = reverseList(oldHead);
+                start.next = newHead;
+                // oldHead翻转后变为尾节点
+                oldHead.next = next;
+                pos = oldHead;
+            } else {
+                //剩余成都不足k时，跳出
+                break;
+            }
+        }
+        return dummyHead.next;
+    }
 
+    /**
+     * 206. Reverse Linked List
+     *
+     * @param head
+     * @return
+     */
+    private static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode mid = slow.next;
+        ListNode fast = mid.next;
+        slow.next = null;
+        while (fast != null) {
+            mid.next = slow;
+            slow = mid;
+            mid = fast;
+            fast = fast.next;
+        }
+        mid.next = slow;
+        return mid;
     }
 
 }
