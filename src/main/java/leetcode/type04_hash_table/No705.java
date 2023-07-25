60687d8d5b1c27bbc2cf94cde82932c28bc0c619
givedrug <givedrug@outlook.com>
2023-07-25 10:54:59 +0800

    705

--- src/main/java/leetcode/type04_hash_table/No705.java
+++ src/main/java/leetcode/type04_hash_table/No705.java
@@ -9,29 +9,118 @@ package leetcode.type04_hash_table;
 public class No705 {
 
     public static void main(String[] args) {
+        MyHashSet myHashSet = new No705().new MyHashSet();
+        // set = [1]
+        myHashSet.add(1);
+        // set = [1, 2]
+        myHashSet.add(2);
+        // return True
+        System.out.println(myHashSet.contains(1));
+        // return False, (not found)
+        System.out.println(myHashSet.contains(3));
+        // set = [1, 2]
+        myHashSet.add(2);
+        // return True
+        System.out.println(myHashSet.contains(2));
+        // set = [1]
+        myHashSet.remove(2);
+        // return False, (already removed)
+        System.out.println(myHashSet.contains(2));
 
+        return;
     }
 
     /**
-     * 思路：
-     * 复杂度：
+     * 思路：使用一个数组存数据，数组元素是一个链表表头，该链表下所有key的hash值相等
+     * 另外，这里只是给出一个最简单的hashSet实现，如果要进行优化，可以从下面几点考虑：
+     * 1）hash函数，可以采用更优的散列函数
+     * 2）链表改为树，参考Java内置HashMap的实现
+     * 3）扩容机制，当元素个数过多时，可以对其进行扩容，降低hash冲突概率
+     * 复杂度：>1
      */
     class MyHashSet {
 
-        public MyHashSet() {
+        private final int SIZE = 16;
+        ListNode[] array;
 
+        public MyHashSet() {
+            array = new ListNode[16];
         }
 
         public void add(int key) {
-
+            int hash = getHash(key);
+            ListNode newNode = new ListNode(key);
+            if (array[hash] == null) {
+                array[hash] = newNode;
+            } else {
+                ListNode pos = array[hash];
+                while (pos != null) {
+                    if (pos.val == key) {
+                        return;
+                    }
+                    pos = pos.next;
+                }
+                newNode.next = array[hash];
+                array[hash] = newNode;
+            }
         }
 
         public void remove(int key) {
-
+            int hash = getHash(key);
+            if (array[hash] != null) {
+                ListNode pre = null;
+                ListNode pos = array[hash];
+                while (pos != null) {
+                    ListNode next = pos.next;
+                    if (pos.val == key) {
+                        if (pre == null) {
+                            array[hash] = next;
+                        } else {
+                            pre.next = next;
+                        }
+                        pos.next = null;
+                        return;
+                    } else {
+                        pre = pos;
+                        pos = next;
+                    }
+                }
+            }
         }
 
         public boolean contains(int key) {
+            int hash = getHash(key);
+            if (array[hash] != null) {
+                ListNode pos = array[hash];
+                while (pos != null) {
+                    if (pos.val == key) {
+                        return true;
+                    }
+                    pos = pos.next;
+                }
+            }
+            return false;
+        }
 
+        private int getHash(int key) {
+            return String.valueOf(key).hashCode() % SIZE;
+        }
+
+        class ListNode {
+            public int val;
+            public ListNode next;
+
+            public ListNode() {
+            }
+
+            public ListNode(int val) {
+                this.val = val;
+            }
+
+            public ListNode(int val, ListNode next) {
+                this.val = val;
+                this.next = next;
+            }
         }
 
     }
