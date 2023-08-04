a826ba262d351b96f6422deb6b8c1104a5207f57
givedrug <givedrug@outlook.com>
2023-08-04 15:36:12 +0800

    744

---
+++ src/main/java/leetcode/type05_binary_search/No744.java
@@ -0,0 +1,47 @@
+package leetcode.type05_binary_search;
+
+import static leetcode.common.FormatConversion.strToCharArray;
+
+/**
+ * 744. Find Smallest Letter Greater Than Target
+ *
+ * @author <a href="https://github.com/givedrug">givedrug</a>
+ * @create 2023-08-04 15:20
+ */
+public class No744 {
+
+    public static void main(String[] args) {
+        // c
+        System.out.println(nextGreatestLetter(strToCharArray("[\"c\",\"f\",\"j\"]"), 'a'));
+        // f
+        System.out.println(nextGreatestLetter(strToCharArray("[\"c\",\"f\",\"j\"]"), 'c'));
+        // x
+        System.out.println(nextGreatestLetter(strToCharArray("[\"x\",\"x\",\"y\",\"y\"]"), 'z'));
+    }
+
+    /**
+     * 思路：二分
+     * 复杂度：log(n)
+     */
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int left = 0, right = letters.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] == target + 1) {
+                return letters[mid];
+            }
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        if (left != letters.length && letters[left] > target) {
+            return letters[left];
+        }
+
+        return letters[0];
+    }
+
+}
